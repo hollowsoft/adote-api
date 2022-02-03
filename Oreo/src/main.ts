6ecefe270c71078b62e6336d9b7f9f1cb33d57c3
Igor Morais <igor.morais@hollowsoft.com>
2022-02-02 22:45:59 -0300

    adding support to fastify

--- Oreo/src/main.ts
+++ Oreo/src/main.ts
@@ -1,8 +1,11 @@
 import { AppModule } from './app.module'
+
 import { NestFactory } from '@nestjs/core'
 
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
+
 const load = async () => {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
 
   await app.listen(4000)
 }
