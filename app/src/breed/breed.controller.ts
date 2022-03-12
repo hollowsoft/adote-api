208b454326c25adff16b7721133edfcb0c0d414a
Igor Morais <igor.morais@hollowsoft.com>
2022-03-12 10:37:17 -0300

    implement breed controller

--- app/src/breed/breed.controller.ts
+++ app/src/breed/breed.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Query } from '@nestjs/common'
 
+import { Kind } from './entity/kind.entity'
 import { Breed } from './entity/breed.entity'
+
+import { BreedService } from './breed.service'
 
 @Controller('breed')
 export class BreedController {
+  constructor(private readonly service: BreedService) {}
+
   @Get()
-  get(): Breed[] {
-    return []
+  get(@Query('kind') kind: Kind): Promise<Breed[]> {
+    return this.service.find(kind)
   }
 }
