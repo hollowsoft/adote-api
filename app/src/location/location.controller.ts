966cbe296291b9ea962b2359402f37034a53a00e
Igor Morais <igor.morais@hollowsoft.com>
2022-03-12 11:55:38 -0300

    implement location controller

--- app/src/location/location.controller.ts
+++ app/src/location/location.controller.ts
@@ -2,10 +2,14 @@ import { Controller, Get, Query } from '@nestjs/common'
 
 import { City } from './entity/city.entity'
 
+import { LocationService } from './location.service'
+
 @Controller('location')
 export class LocationController {
+  constructor(private readonly service: LocationService) {}
+
   @Get('search')
-  search(@Query('term') term: string): City[] {
-    return []
+  search(@Query('term') term: string): Promise<City[]> {
+    return this.service.find(term)
   }
 }
