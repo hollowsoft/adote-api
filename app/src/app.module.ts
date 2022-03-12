577aeb4c81f535579e79b69cf2d608668ee4221f
Igor Morais <igor.morais@hollowsoft.com>
2022-03-12 10:37:48 -0300

    adding breed module to main module

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { UserModule } from './user/user.module'
+import { BreedModule } from './breed/breed.module'
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule],
+  imports: [TypeOrmModule.forRoot(), UserModule, BreedModule],
 })
 export class AppModule {}
