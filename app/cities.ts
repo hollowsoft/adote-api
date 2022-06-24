ef22ced69755f3f685f27563c685bd45532121ff
4vm <amadeumrzr@gmail.com>
2022-06-24 08:37:11 -0300

    added cities in matoGrosso

--- app/cities.ts
+++ app/cities.ts
@@ -6732,3 +6732,423 @@ const minasGerais = [
     name: 'Wenceslau Braz',
   },
 ]
+
+const matoGrosso = [
+  {
+    name: 'Acorizal',
+  },
+  {
+    name: 'Água Boa',
+  },
+  {
+    name: 'Alta Floresta',
+  },
+  {
+    name: 'Alto Araguaia',
+  },
+  {
+    name: 'Alto Boa Vista',
+  },
+  {
+    name: 'Alto Garças',
+  },
+  {
+    name: 'Alto Paraguai',
+  },
+  {
+    name: 'Alto Taquari',
+  },
+  {
+    name: 'Apiacás',
+  },
+  {
+    name: 'Araguaiana',
+  },
+  {
+    name: 'Araguainha',
+  },
+  {
+    name: 'Araputanga',
+  },
+  {
+    name: 'Arenápolis',
+  },
+  {
+    name: 'Aripuanã',
+  },
+  {
+    name: 'Barão de Melgaço',
+  },
+  {
+    name: 'Barra do Bugres',
+  },
+  {
+    name: 'Barra do Garças',
+  },
+  {
+    name: 'Bom Jesus do Araguaia',
+  },
+  {
+    name: 'Brasnorte',
+  },
+  {
+    name: 'Cáceres',
+  },
+  {
+    name: 'Campinápolis',
+  },
+  {
+    name: 'Campo Novo do Parecis',
+  },
+  {
+    name: 'Campo Verde',
+  },
+  {
+    name: 'Campos de Júlio',
+  },
+  {
+    name: 'Canabrava do Norte',
+  },
+  {
+    name: 'Canarana',
+  },
+  {
+    name: 'Carlinda',
+  },
+  {
+    name: 'Castanheira',
+  },
+  {
+    name: 'Chapada dos Guimarães',
+  },
+  {
+    name: 'Cláudia',
+  },
+  {
+    name: 'Cocalinho',
+  },
+  {
+    name: 'Colíder',
+  },
+  {
+    name: 'Colniza',
+  },
+  {
+    name: 'Comodoro',
+  },
+  {
+    name: 'Confresa',
+  },
+  {
+    name: 'Conquista dOeste',
+  },
+  {
+    name: 'Cotriguaçu',
+  },
+  {
+    name: 'Curvelândia',
+  },
+  {
+    name: 'Cuiabá',
+  },
+  {
+    name: 'Denise',
+  },
+  {
+    name: 'Diamantino',
+  },
+  {
+    name: 'Dom Aquino',
+  },
+  {
+    name: 'Feliz Natal',
+  },
+  {
+    name: 'Figueirópolis dOeste',
+  },
+  {
+    name: 'Gaúcha do Norte',
+  },
+  {
+    name: 'General Carneiro',
+  },
+  {
+    name: 'Glória dOeste',
+  },
+  {
+    name: 'Guarantã do Norte',
+  },
+  {
+    name: 'Guiratinga',
+  },
+  {
+    name: 'Indiavaí',
+  },
+  {
+    name: 'Itaúba',
+  },
+  {
+    name: 'Itiquira',
+  },
+  {
+    name: 'Jaciara',
+  },
+  {
+    name: 'Jangada',
+  },
+  {
+    name: 'Jauru',
+  },
+  {
+    name: 'Juara',
+  },
+  {
+    name: 'Juína',
+  },
+  {
+    name: 'Juruena',
+  },
+  {
+    name: 'Juscimeira',
+  },
+  {
+    name: 'Lambari dOeste',
+  },
+  {
+    name: 'Lucas do Rio Verde',
+  },
+  {
+    name: 'Luciára',
+  },
+  {
+    name: 'Marcelândia',
+  },
+  {
+    name: 'Matupá',
+  },
+  {
+    name: 'Mirassol dOeste',
+  },
+  {
+    name: 'Nobres',
+  },
+  {
+    name: 'Nortelândia',
+  },
+  {
+    name: 'Nossa Senhora do Livramento',
+  },
+  {
+    name: 'Nova Bandeirantes',
+  },
+  {
+    name: 'Nova Brasilândia',
+  },
+  {
+    name: 'Nova Canãa do Norte',
+  },
+  {
+    name: 'Nova Guarita',
+  },
+  {
+    name: 'Nova Lacerda',
+  },
+  {
+    name: 'Nova Marilândia',
+  },
+  {
+    name: 'Nova Maringá',
+  },
+  {
+    name: 'Nova Monte Verde',
+  },
+  {
+    name: 'Nova Mutum',
+  },
+  {
+    name: 'Nova Nazaré',
+  },
+  {
+    name: 'Nova Olímpia',
+  },
+  {
+    name: 'Nova Santa Helena',
+  },
+  {
+    name: 'Nova Ubiratã',
+  },
+  {
+    name: 'Nova Xavantina',
+  },
+  {
+    name: 'Novo Horizonte do Norte',
+  },
+  {
+    name: 'Novo Mundo',
+  },
+  {
+    name: 'Novo Santo Antônio',
+  },
+  {
+    name: 'Novo São Joaquim',
+  },
+  {
+    name: 'Paranaíta',
+  },
+  {
+    name: 'Paranatinga',
+  },
+  {
+    name: 'Pedra Preta',
+  },
+  {
+    name: 'Peixoto de Azevedo',
+  },
+  {
+    name: 'Planalto da Serra',
+  },
+  {
+    name: 'Poconé',
+  },
+  {
+    name: 'Pontal do Araguaia',
+  },
+  {
+    name: 'Ponte Branca',
+  },
+  {
+    name: 'Pontes e Lacerda',
+  },
+  {
+    name: 'Porto Alegre do Norte',
+  },
+  {
+    name: 'Porto dos Gaúchos',
+  },
+  {
+    name: 'Porto Esperidião',
+  },
+  {
+    name: 'Porto Estrela',
+  },
+  {
+    name: 'Poxoréo',
+  },
+  {
+    name: 'Primavera do Leste',
+  },
+  {
+    name: 'Querência',
+  },
+  {
+    name: 'Reserva do Cabaçal',
+  },
+  {
+    name: 'Ribeirão Cascalheira',
+  },
+  {
+    name: 'Ribeirãozinho',
+  },
+  {
+    name: 'Rio Branco',
+  },
+  {
+    name: 'Rondolândia',
+  },
+  {
+    name: 'Rondonópolis',
+  },
+  {
+    name: 'Rosário Oeste',
+  },
+  {
+    name: 'Salto do Céu',
+  },
+  {
+    name: 'Santa Carmem',
+  },
+  {
+    name: 'Santa Cruz do Xingu',
+  },
+  {
+    name: 'Santa Rita do Trivelato',
+  },
+  {
+    name: 'Santa Terezinha',
+  },
+  {
+    name: 'Santo Afonso',
+  },
+  {
+    name: 'Santo Antônio do Leste',
+  },
+  {
+    name: 'Santo Antônio do Leverger',
+  },
+  {
+    name: 'São Félix do Araguaia',
+  },
+  {
+    name: 'São José do Povo',
+  },
+  {
+    name: 'São José do Rio Claro',
+  },
+  {
+    name: 'São José do Xingu',
+  },
+  {
+    name: 'São José dos Quatro Marcos',
+  },
+  {
+    name: 'São Pedro da Cipa',
+  },
+  {
+    name: 'Sapezal',
+  },
+  {
+    name: 'Serra Nova Dourada',
+  },
+  {
+    name: 'Sinop',
+  },
+  {
+    name: 'Sorriso',
+  },
+  {
+    name: 'Tabaporã',
+  },
+  {
+    name: 'Tangará da Serra',
+  },
+  {
+    name: 'Tapurah',
+  },
+  {
+    name: 'Terra Nova do Norte',
+  },
+  {
+    name: 'Tesouro',
+  },
+  {
+    name: 'Torixoréu',
+  },
+  {
+    name: 'União do Sul',
+  },
+  {
+    name: 'Vale de São Domingos',
+  },
+  {
+    name: 'Várzea Grande',
+  },
+  {
+    name: 'Vera',
+  },
+  {
+    name: 'Vila Bela da Santíssima Trindade',
+  },
+  {
+    name: 'Vila Rica',
+  },
+]
