69e6bd3291fe9a38b815efec16c33dca524c4ab3
4vm <amadeumrzr@gmail.com>
2022-06-25 10:15:21 -0300

    added cities in roraima

--- app/cities.ts
+++ app/cities.ts
@@ -13350,3 +13350,51 @@ const rondonia = [
     name: 'Vilhena',
   },
 ]
+
+const roraima = [
+  {
+    name: 'Alto Alegre',
+  },
+  {
+    name: 'Amajari',
+  },
+  {
+    name: 'Boa Vista',
+  },
+  {
+    name: 'Bonfim',
+  },
+  {
+    name: 'Cantá',
+  },
+  {
+    name: 'Caracaraí',
+  },
+  {
+    name: 'Caroebe',
+  },
+  {
+    name: 'Iracema',
+  },
+  {
+    name: 'Mucajaí',
+  },
+  {
+    name: 'Normandia',
+  },
+  {
+    name: 'Pacaraima',
+  },
+  {
+    name: 'Rorainópolis',
+  },
+  {
+    name: 'São João da Baliza',
+  },
+  {
+    name: 'São Luiz',
+  },
+  {
+    name: 'Uiramutã',
+  },
+]
