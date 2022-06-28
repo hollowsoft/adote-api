77a21af5cc2dcb404189367c1e3f35eeb8fb12d3
Igor Morais <igor.morais@hollowsoft.com>
2022-06-27 23:03:35 -0300

    implement migration support

---
+++ app/src/migration/1656380584269-create_breed_content.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class CreateBreedContent implements MigrationInterface {
+  public async up(query: QueryRunner) {
+
+  }
+
+  public async down(query: QueryRunner) {
+
+  }
+}
---
+++ app/src/migration/1656380637233-create_state_content.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class CreateStateContent implements MigrationInterface {
+  public async up(query: QueryRunner) {
+
+  }
+
+  public async down(query: QueryRunner) {
+    
+  }
+}
