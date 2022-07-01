f90725179ea8ab3033f755b9d77ffb3494ec308c
4vm <amadeumrzr@gmail.com>
2022-07-01 10:15:13 -0300

    implement create schema migration

--- app/src/env.ts
+++ app/src/env.ts
@@ -1,24 +1,20 @@
-import { Breed } from './breed/entity/breed.entity'
+import { DataSource } from 'typeorm'
 
-const config = {
+const source = new DataSource({
   type: 'postgres',
   host: 'ec2-3-230-238-86.compute-1.amazonaws.com',
   port: 5432,
   username: 'twkmggffahkiib',
   password: 'c950083e03f23d41a57afd73aeabcda992695ea8e4a19447af182bc109029a7c',
   database: 'd48qme3gauv0gi',
-  entities: [Breed],
-  synchronize: true,
-  migrations: ['migration/*.ts'],
+  entities: ['src/**/*.entity.ts'],
+  migrations: ['src/migration/*.ts'],
   migrationsTableName: 'schema',
-  cli: {
-    migrationsDir: 'src/migration',
-  },
   extra: {
     ssl: {
       rejectUnauthorized: false,
     },
   },
-}
+})
 
-export default config
+export default source
