bd51af170c58a02bedb4843164e35edd29782cdc
4vm <amadeumrzr@gmail.com>
2022-07-07 12:55:53 -0300

    add country.seed.ts, implemented create_country_content

--- app/src/migration/1656380620612-create_country_content.ts
+++ app/src/migration/1656380620612-create_country_content.ts
@@ -1,11 +1,13 @@
 import { MigrationInterface, QueryRunner } from 'typeorm'
+import { Country } from 'src/location/entity/country.entity'
+import { countries } from 'seeds/country.seed'
 
 export class CreateCountryContent implements MigrationInterface {
   public async up(query: QueryRunner) {
-
+    await query.manager.save(Country, countries)
   }
 
-  public async down(queryRunner: QueryRunner) {
-    
+  public async down(query: QueryRunner) {
+    await query.dropColumn('country', 'countries')
   }
 }
