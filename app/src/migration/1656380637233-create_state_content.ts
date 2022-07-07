ea57f68ebbebcb8e4bc7be63493fd8c7032a0de1
4vm <amadeumrzr@gmail.com>
2022-07-07 13:02:29 -0300

    add state.seed.ts, implemented create_state_content

--- app/src/migration/1656380637233-create_state_content.ts
+++ app/src/migration/1656380637233-create_state_content.ts
@@ -1,11 +1,13 @@
 import { MigrationInterface, QueryRunner } from 'typeorm'
+import { State } from 'src/location/entity/state.entity'
+import { states } from 'seeds/state.seed'
 
 export class CreateStateContent implements MigrationInterface {
   public async up(query: QueryRunner) {
-
+    await query.manager.save(State, states)
   }
 
   public async down(query: QueryRunner) {
-    
+    await query.dropColumn('state', 'states')
   }
 }
