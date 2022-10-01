cd5469ef354cbeeba9670dc3c056acc4d352fb6e
Igor Morais <igor.morais@hollowsoft.com>
2022-09-30 22:44:18 -0300

    refactor post service

---
+++ app/src/module/post/response/index.ts
@@ -0,0 +1,7 @@
+import { ListPostResponse } from './list.post.response'
+import { CreatePostResponse } from './create.post.response'
+
+export {
+  ListPostResponse,
+  CreatePostResponse
+}
--- app/src/module/post/service/create.post.service.ts
+++ app/src/module/post/service/create.post.service.ts
@@ -3,13 +3,18 @@ import { Injectable } from '@nestjs/common'
 import { Post } from '../entity/post.entity'
 import { PostRepository } from '../post.repository'
 
+import { CreatePostRequest } from '../request'
+import { CreatePostResponse } from '../response'
+
 @Injectable()
 export class CreatePostService {
   constructor(private readonly repository: PostRepository) {}
 
-  run(): Promise<Post> {
-    return this.repository.save(new Post({
+  async run(request: CreatePostRequest): Promise<CreatePostResponse> {
+    const post = await this.repository.save(new Post({
       
     }))
+
+    return new CreatePostResponse(post)
   }
 }
