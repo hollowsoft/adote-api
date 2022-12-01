de1b97049d7bac21e6957e6c63c6f17ad271d2e2
Igor Morais <igor.morais@hollowsoft.com>
2022-12-01 13:31:36 -0300

    remove duplicate history response

--- app/src/module/history/service/list.history.service.ts
+++ app/src/module/history/service/list.history.service.ts
@@ -3,19 +3,20 @@ import { Injectable } from '@nestjs/common'
 import { HistoryRepository } from '../history.repository'
 
 import { ListHistoryRequest } from '../request'
-import { ListHistoryResponse } from '../response'
+
+import { HistoryResponse } from '../response'
 
 @Injectable()
 export class ListHistoryService {
   constructor(private readonly repository: HistoryRepository) {}
 
-  async run(_: ListHistoryRequest): Promise<ListHistoryResponse[]> {
+  async run(_: ListHistoryRequest): Promise<HistoryResponse[]> {
     const list = await this.repository.all({
       order: {
         create: 'desc'
       }
     })
 
-    return list.map((history) => new ListHistoryResponse(history))
+    return list.map((history) => new HistoryResponse(history))
   }
 }
