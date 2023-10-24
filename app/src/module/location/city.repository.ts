fd7a3f1c21d0ea1ceb327f312f116029eeda5f10
Marcos Flavio <mfghost69@gmail.com>
2023-10-24 14:49:22 -0300

    Feature :add ICityRepository interface

--- app/src/module/location/city.repository.ts
+++ app/src/module/location/city.repository.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@nestjs/common'
+
 import { InjectRepository } from '@nestjs/typeorm'
 
-import {
-  Repository,
-  FindManyOptions
-} from 'typeorm'
+import { Repository, FindManyOptions } from 'typeorm'
 
 import { City } from './entity/city.entity'
 
+import { ICityRepository } from './city.repository.interface'
+
 @Injectable()
-export class CityRepository {
+export class CityRepository implements ICityRepository {
   constructor(@InjectRepository(City) private readonly repository: Repository<City>) {}
 
   all(option?: FindManyOptions<City>): Promise<City[]> {
