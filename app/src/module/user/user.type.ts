9a6ae28233b8a480fd2f55bba120fcdddcdb94db
Igor Morais <igor.morais@hollowsoft.com>
2024-03-20 18:24:32 -0300

    add user type

---
+++ app/src/module/user/user.type.ts
@@ -0,0 +1,56 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+
+import { Post } from '@/module/post/post.type'
+import { Location } from '@/module/location/location.type'
+
+export enum Role {
+  Admin = 'admin',
+  Member = 'member'
+}
+
+@Schema()
+export class Contact {
+  @Prop(String)
+  mail?: string
+
+  @Prop(String)
+  phone?: string
+
+  @Prop(String)
+  social?: string
+}
+
+@Schema()
+export class User {
+  @Prop(String)
+  mail: string
+
+  @Prop(String)
+  name?: string
+
+  @Prop(String)
+  image?: string
+
+  @Prop(String)
+  description?: string
+
+  @Prop([Post])
+  fav: Post[]
+
+  @Prop([Post])
+  post: Post[]
+
+  @Prop(Contact)
+  contact?: Contact
+
+  @Prop(Location)
+  location?: Location
+
+  @Prop(Role)
+  role: Role
+
+  @Prop(Boolean)
+  enable: boolean
+}
+
+export const UserSchema = SchemaFactory.createForClass(User)
