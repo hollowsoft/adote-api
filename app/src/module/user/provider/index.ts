94442c135cf32bb0957c1b5a4ea8eb472a002066
Igor Morais <igor.morais@hollowsoft.com>
2024-03-22 01:19:19 -0300

    add user provider

--- app/src/module/user/provider/index.ts
+++ app/src/module/user/provider/index.ts
@@ -1,7 +1,30 @@
-import { GetCurrent } from './get.current'
-import { GetUser } from './get.user'
-import { ListUser } from './list.user.service'
-import { SetImageUser } from './set.image.user'
-import { UpdateUser } from './update.user.service'
+import { Injectable } from '@nestjs/common'
 
-export { GetCurrent, GetUser, ListUser, SetImageUser, UpdateUser }
+import { GetUser } from './get.user'
+import { GetCurrent } from './get.current'
+import { ListUser } from './list.user.service'
+import { UpdateUser } from './update.user.service'
+import { SetImageUser } from './set.image.user'
+
+export enum Action {
+  Get,
+  Current,
+  List,
+  Update,
+  Image
+}
+
+@Injectable()
+export class UserProvider {
+  action: [GetUser, GetCurrent, ListUser, UpdateUser, SetImageUser]
+
+  constructor(
+    private readonly get: GetUser,
+    private readonly current: GetCurrent,
+    private readonly list: ListUser,
+    private readonly update: UpdateUser,
+    private readonly image: SetImageUser
+  ) {
+    this.action = [this.get, this.current, this.list, this.update, this.image]
+  }
+}
