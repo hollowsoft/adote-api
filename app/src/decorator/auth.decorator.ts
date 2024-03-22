3c9da97f05bb960c56464519781944482bf9626a
Igor Morais <igor.morais@hollowsoft.com>
2024-03-22 01:53:03 -0300

    update auth decorator

--- app/src/decorator/auth.decorator.ts
+++ app/src/decorator/auth.decorator.ts
@@ -1,6 +1,6 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common'
 
-import { Token } from '../type/token.type'
+import { Token } from '@/type/token.type'
 
 export const Auth = createParamDecorator((_: unknown, context: ExecutionContext): Token => {
   const { user } = context.switchToHttp().getRequest()
