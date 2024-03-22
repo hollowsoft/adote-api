56ec927476ef0daeaaa0e5de05b76f3d088a75e7
Igor Morais <igor.morais@hollowsoft.com>
2024-03-22 01:17:54 -0300

    add post provider

--- app/src/module/post/provider/index.ts
+++ app/src/module/post/provider/index.ts
@@ -1,8 +1,33 @@
+import { Injectable } from '@nestjs/common'
+
 import { GetPost } from './get.post'
+import { ListPost } from './list.post'
 import { CreatePost } from './create.post'
 import { UpdatePost } from './update.post'
-import { RemovePost } from './remove.post'
-import { ListPost } from './list.post'
 import { PublishPost } from './publish.post'
+import { RemovePost } from './remove.post'
 
-export { GetPost, CreatePost, UpdatePost, RemovePost, ListPost, PublishPost }
+export enum Action {
+  Get,
+  List,
+  Create,
+  Update,
+  Publish,
+  Remove
+}
+
+@Injectable()
+export class PostProvider {
+  action: [GetPost, ListPost, CreatePost, UpdatePost, PublishPost, RemovePost]
+
+  constructor(
+    private readonly get: GetPost,
+    private readonly list: ListPost,
+    private readonly create: CreatePost,
+    private readonly update: UpdatePost,
+    private readonly publish: PublishPost,
+    private readonly remove: RemovePost
+  ) {
+    this.action = [this.get, this.list, this.create, this.update, this.publish, this.remove]
+  }
+}
