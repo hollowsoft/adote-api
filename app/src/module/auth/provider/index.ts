723d2820efb6c156d88b476ea37d9467cb4c4191
Igor Morais <igor.morais@hollowsoft.com>
2024-03-22 00:12:22 -0300

    add auth provider

--- app/src/module/auth/provider/index.ts
+++ app/src/module/auth/provider/index.ts
@@ -1,5 +1,24 @@
+import { Injectable } from '@nestjs/common'
+
 import { MailAuth } from './mail.auth'
 import { RenewAuth } from './renew.auth'
 import { VerifyAuth } from './verify.auth'
 
-export { MailAuth, RenewAuth, VerifyAuth }
+export enum Action {
+  Mail,
+  Renew,
+  Verify
+}
+
+@Injectable()
+export class AuthProvider {
+  action: [MailAuth, RenewAuth, VerifyAuth]
+
+  constructor(
+    private readonly mail: MailAuth,
+    private readonly renew: RenewAuth,
+    private readonly verify: VerifyAuth
+  ) {
+    this.action = [this.mail, this.renew, this.verify]
+  }
+}
