824784dba3419cd2b77fac2824e262871177ccc0
Igor Morais <igor.morais@hollowsoft.com>
2024-03-22 00:13:20 -0300

    add fav provider

--- app/src/module/fav/provider/index.ts
+++ app/src/module/fav/provider/index.ts
@@ -1,5 +1,24 @@
+import { Injectable } from '@nestjs/common'
+
 import { AddFav } from './add.fav'
 import { ListFav } from './list.fav'
 import { RemoveFav } from './remove.fav'
 
-export { AddFav, ListFav, RemoveFav }
+export enum Action {
+  Add,
+  List,
+  Remove
+}
+
+@Injectable()
+export class FavProvider {
+  action: [AddFav, ListFav, RemoveFav]
+
+  constructor(
+    private readonly add: AddFav,
+    private readonly list: ListFav,
+    private readonly remove: RemoveFav
+  ) {
+    this.action = [this.add, this.list, this.remove]
+  }
+}
