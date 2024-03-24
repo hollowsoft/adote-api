ada4d6e9f7f6b1b8f31298d216b98e3ec9aa02f7
Igor Morais <igor.morais@hollowsoft.com>
2024-03-24 20:48:25 -0300

    rename functions

--- app/src/helper/environment.ts
+++ app/src/helper/environment.ts
@@ -1,3 +1,3 @@
-export const isDev = (): boolean => process.env.NODE_ENV === 'development'
+export const isProduction = (): boolean => process.env.NODE_ENV === 'production'
 
-export const isProd = (): boolean => process.env.NODE_ENV === 'production'
+export const isDevelopment = (): boolean => process.env.NODE_ENV === 'development'
