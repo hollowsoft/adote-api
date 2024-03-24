fd2acebb28b70ec5f2bbcba731cd234b245eb4d0
Igor Morais <igor.morais@hollowsoft.com>
2024-03-24 15:26:03 -0300

    update auth module

--- app/src/module/auth/auth.controller.ts
+++ app/src/module/auth/auth.controller.ts
@@ -1,41 +1,41 @@
 import { Post, Body, HttpCode, UseGuards, Controller } from '@nestjs/common'
 
-import { Auth } from '../../decorator/auth.decorator'
-import { Public } from '../../decorator/public.decorator'
+import { Auth } from '@/decorator/auth.decorator'
+import { Public } from '@/decorator/public.decorator'
 
-import { Token } from '../../type/token.type'
+import { Token } from '@/type/token.type'
 import { TokenRenewGuard } from './guard/token.renew.guard'
+
+import { Action, AuthProvider } from './provider'
 
 import { AuthRequest, VerifyRequest } from './auth.request'
 import { AuthResponse, TokenResponse } from './auth.response'
-
-import { Action, AuthProvider } from './auth.provider'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly provider: AuthProvider) {}
 
-  @Public()
   @Post()
+  @Public()
   @HttpCode(200)
   mail(@Body() request: AuthRequest): Promise<AuthResponse> {
-    return this.provider[Action.MailAuth].run(request)
+    return this.provider[Action.Mail].run(request)
   }
 
-  @Public()
   @Post('verify')
+  @Public()
   @HttpCode(200)
   code(@Body() request: VerifyRequest): Promise<TokenResponse> {
-    return this.provider[Action.VerifyAuth].run(request)
+    return this.provider[Action.Verify].run(request)
   }
 
+  @Post('token')
   @Public()
   @UseGuards(TokenRenewGuard)
-  @Post('token')
   @HttpCode(200)
   token(@Auth() token: Token): Promise<TokenResponse> {
-    const { sub } = token
+    const { sub: id } = token
 
-    return this.provider[Action.RenewAuth].run(sub)
+    return this.provider[Action.Renew].run(id)
   }
 }
--- app/src/module/auth/auth.module.ts
+++ app/src/module/auth/auth.module.ts
@@ -1,12 +1,10 @@
 import { APP_GUARD } from '@nestjs/core'
 
 import { Module } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
-
+import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
-import { JwtService, JwtModule } from '@nestjs/jwt'
 
-import { UserModule } from '../user/user.module'
+import { UserModule } from '@/module/user/user.module'
 
 import { TokenGuard } from './guard/token.guard'
 import { PermissionGuard } from './guard/permission.guard'
@@ -14,22 +12,23 @@ import { PermissionGuard } from './guard/permission.guard'
 import { TokenStrategy } from './strategy/token.strategy'
 import { TokenRenewStrategy } from './strategy/token.renew.strategy'
 
-import { AuthProvider } from './auth.provider'
-
-import { UserRepository } from '../user/user.repository'
+import { AuthProvider } from './provider'
 
 import { AuthController } from './auth.controller'
 
+const TokenGuardProvider = {
+  provide: APP_GUARD,
+  useClass: TokenGuard
+}
+
+const PermissionGuardProvider = {
+  provide: APP_GUARD,
+  useClass: PermissionGuard
+}
+
 @Module({
   imports: [JwtModule, PassportModule, UserModule],
-  providers: [
-    TokenStrategy,
-    TokenRenewStrategy,
-    { provide: APP_GUARD, useClass: TokenGuard },
-    { provide: APP_GUARD, useClass: PermissionGuard },
-    AuthProvider,
-    UserRepository
-  ],
+  providers: [AuthProvider, TokenGuardProvider, PermissionGuardProvider, TokenStrategy, TokenRenewStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
