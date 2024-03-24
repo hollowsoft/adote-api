84a8fa70b592a4e7579140d16eda0fc8a5eafbf9
Igor Morais <igor.morais@hollowsoft.com>
2024-03-24 20:49:03 -0300

    rename provider

--- app/src/module/post/provider/index.ts
+++ app/src/module/post/provider/index.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common'
 import { GetPost } from './get.post'
 import { ListPost } from './list.post'
 import { CreatePost } from './create.post'
-import { UpdatePost } from './update.post'
+import { PatchPost } from './patch.post'
 import { PublishPost } from './publish.post'
 import { RemovePost } from './remove.post'
 
@@ -11,23 +11,23 @@ export enum Action {
   Get,
   List,
   Create,
-  Update,
+  Patch,
   Publish,
   Remove
 }
 
 @Injectable()
 export class PostProvider {
-  action: [GetPost, ListPost, CreatePost, UpdatePost, PublishPost, RemovePost]
+  action: [GetPost, ListPost, CreatePost, PatchPost, PublishPost, RemovePost]
 
   constructor(
     private readonly get: GetPost,
     private readonly list: ListPost,
     private readonly create: CreatePost,
-    private readonly update: UpdatePost,
+    private readonly patch: PatchPost,
     private readonly publish: PublishPost,
     private readonly remove: RemovePost
   ) {
-    this.action = [this.get, this.list, this.create, this.update, this.publish, this.remove]
+    this.action = [this.get, this.list, this.create, this.patch, this.publish, this.remove]
   }
 }
