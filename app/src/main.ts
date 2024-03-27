6399dce9813ed5c758139c28f1b437f0d2e4617d
Igor Morais <igor.morais@hollowsoft.com>
2024-03-26 21:58:12 -0300

    update main

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,12 +1,7 @@
-import 'reflect-metadata'
-
 import { NestFactory } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
 
-import {
-  FastifyAdapter,
-  NestFastifyApplication
-} from '@nestjs/platform-fastify'
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
 
 import helmet from '@fastify/helmet'
 import compress from '@fastify/compress'
@@ -15,15 +10,17 @@ import { AppModule } from './app.module'
 
 (async () => {
   const application = await NestFactory.create<NestFastifyApplication>(
-    AppModule, new FastifyAdapter({ logger: false }), { cors: true })
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+    { cors: true }
+  )
 
   await application.register(helmet)
   await application.register(compress)
 
-  application.useGlobalPipes(new ValidationPipe({ transform: true }))
-
   application.setGlobalPrefix('/api')
+
+  application.useGlobalPipes(new ValidationPipe({ transform: true }))
 
   await application.listen(process.env.PORT ?? 8000, '0.0.0.0')
 })()
-
