8cba986ce3d749b3ace4609582df2b46c72b7f51
Igor Morais <igor.morais@hollowsoft.com>
2024-03-26 23:29:19 -0300

    update environment helper

--- app/src/helper/environment.ts
+++ app/src/helper/environment.ts
@@ -1,3 +1,3 @@
-export const isProduction = (): boolean => process.env.NODE_ENV === 'production'
+export const isProduction: boolean = process.env.NODE_ENV === 'production'
 
-export const isDevelopment = (): boolean => process.env.NODE_ENV === 'development'
+export const isDevelopment: boolean = process.env.NODE_ENV === 'development'
