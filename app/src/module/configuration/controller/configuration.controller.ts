24ffd4ac3d18b5738205d7505b5f503a53a30fbf
Marcos Flavio <mfghost69@gmail.com>
2024-07-05 00:56:48 -0300

    Fix: Removing the Public decorator from Configuration controller

--- app/src/module/configuration/controller/configuration.controller.ts
+++ app/src/module/configuration/controller/configuration.controller.ts
@@ -2,14 +2,12 @@ import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common'
 import { Action, ConfigurationProvider } from '../configuration.provider'
 import { Role } from '@/module/user/user.type'
 import { Permission } from '@/decorator/permission.decorator'
-import { Public } from '@/decorator/public.decorator'
 
 @Controller('load')
 export class ConfigurationController {
   constructor(private readonly provider: ConfigurationProvider) {}
 
   @Permission(Role.Admin)
-  @Public()
   @Get()
   @HttpCode(HttpStatus.OK)
   async loadLocation(): Promise<HttpStatus> {
