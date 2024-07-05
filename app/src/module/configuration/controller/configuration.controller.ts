92ebfe48ce4f4c419c78307c566134a9a521cc6f
Marcos Flavio <mfghost69@gmail.com>
2024-07-05 16:34:47 -0300

    Fix: change controller to a Post instead of Get

--- app/src/module/configuration/controller/configuration.controller.ts
+++ app/src/module/configuration/controller/configuration.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common'
+import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common'
 import { Action, ConfigurationProvider } from '../configuration.provider'
 import { Role } from '@/module/user/user.type'
 import { Permission } from '@/decorator/permission.decorator'
@@ -8,7 +8,7 @@ export class ConfigurationController {
   constructor(private readonly provider: ConfigurationProvider) {}
 
   @Permission(Role.Admin)
-  @Get()
+  @Post()
   @HttpCode(HttpStatus.OK)
   async loadLocation(): Promise<HttpStatus> {
     if ((await this.provider.action[Action.LoadLocation].run()) != null) {
