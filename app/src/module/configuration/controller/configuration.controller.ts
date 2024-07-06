4cd3e1e7c06fc417c7bec50b04d23a77468f2ec0
Marcos Flavio <mfghost69@gmail.com>
2024-07-06 12:23:50 -0300

    Fix: Change the endpoints to /configuration and /location

--- app/src/module/configuration/controller/configuration.controller.ts
+++ app/src/module/configuration/controller/configuration.controller.ts
@@ -3,16 +3,14 @@ import { Action, ConfigurationProvider } from '../configuration.provider'
 import { Role } from '@/module/user/user.type'
 import { Permission } from '@/decorator/permission.decorator'
 
-@Controller('load')
+@Controller('/configuration')
 export class ConfigurationController {
   constructor(private readonly provider: ConfigurationProvider) {}
 
   @Permission(Role.Admin)
-  @Post()
+  @Post('/location')
   @HttpCode(HttpStatus.OK)
-  async loadLocation(): Promise<HttpStatus> {
-    if ((await this.provider.action[Action.LoadLocation].run()) != null) {
-      return HttpStatus.OK
-    }
+  async loadLocation(): Promise<void> {
+    await this.provider.action[Action.LoadLocation].run()
   }
 }
