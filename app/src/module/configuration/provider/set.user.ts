e715e46384c6c0876b4401f147c94d6df34dcc0e
Marcos Flavio <mfghost69@gmail.com>
2024-07-10 12:22:17 -0300

    Fix: adding param casting for Role

--- app/src/module/configuration/provider/set.user.ts
+++ app/src/module/configuration/provider/set.user.ts
@@ -1,14 +1,20 @@
 import { UserRepository } from '@/module/user/user.repository'
 import user from '../../../../user.json'
 import { Injectable } from '@nestjs/common'
+import { Role } from '@/module/user/user.type'
 
 @Injectable()
 export class SetUser {
   constructor(private repository: UserRepository) {}
 
   async run() {
+    const usersArray = user.map((user) => ({
+      ...user,
+      role: user.role as Role
+    }))
+
     await this.repository.remove()
 
-    await this.repository.saveMany(user)
+    await this.repository.saveMany(usersArray)
   }
 }
