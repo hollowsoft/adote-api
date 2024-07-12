a1c3bcee9819eaf34776a73264e6acab2e9a0257
Marcos Flavio <mfghost69@gmail.com>
2024-07-12 00:17:23 -0300

    Feat: Read and sort cats/dogs.json

--- app/src/module/configuration/provider/set.breed.provider.ts
+++ app/src/module/configuration/provider/set.breed.provider.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common'
 
-import breed from '@/module/breed/breed.json'
+import dogs from '@/module/breed/dogs.json'
+import cats from '@/module/breed/cats.json'
 
 import { BreedRepository } from '@/module/breed/breed.repository'
 import { Kind } from '@/module/breed/breed.type'
@@ -10,10 +11,20 @@ export class SetBreedProvider {
   constructor(private repository: BreedRepository) {}
 
   async run(): Promise<void> {
-    const breedArray = breed.map((breed) => ({
-      ...breed,
-      kind: Kind[breed.kind]
-    }))
+    const breedArray = dogs
+      .map((dog) => ({
+        ...dog,
+        kind: Kind['Dog']
+      }))
+      .concat(
+        cats.map((cat) => ({
+          ...cat,
+          kind: Kind['Cat']
+        }))
+      )
+      .sort((a, b) => {
+        return a.name.localeCompare(b.name)
+      })
 
     await this.repository.remove()
 
