1647f7770c4e4bdde271f6eae7b8dacd9c7f2196
Marcos Flavio <mfghost69@gmail.com>
2024-08-22 12:42:55 -0300

    Fix: commit repository

--- app/src/module/post/post.repository.ts
+++ app/src/module/post/post.repository.ts
@@ -3,25 +3,29 @@ import { InjectModel } from '@nestjs/mongoose'
 
 import { Model } from 'mongoose'
 
-import { Post } from './post.type'
+import { CreatePost, Post, PostDocument } from './post.type'
 
 @Injectable()
 export class PostRepository {
   constructor(@InjectModel(Post.name) private model: Model<Post>) {}
 
-  list(): Promise<Post[]> {
+  list(): Promise<PostDocument[]> {
     return this.model.find().exec()
   }
 
-  find(): Promise<Post> {
+  find(): Promise<PostDocument> {
     return this.model.findById('').exec()
   }
 
-  save(post: Post): Promise<Post> {
-    return this.model.create(post)
+  async save(post: CreatePost): Promise<PostDocument> {
+    return this.model
+      .create(post)
+      .then((model) =>
+        model.populate([{ path: 'user' }, { path: 'location' }, { path: 'pet.breed', model: 'Breed' }])
+      )
   }
 
-  remove(): Promise<Post> {
+  remove(): Promise<PostDocument> {
     return this.model.findByIdAndDelete('').exec()
   }
 }
--- app/src/module/post/provider/create.post.provider.ts
+++ app/src/module/post/provider/create.post.provider.ts
@@ -9,8 +9,14 @@ export class CreatePostProvider {
   constructor(private readonly repository: PostRepository) {}
 
   async run(request: CreatePostRequest, user: string): Promise<PostResponse> {
+    const post = {
+      ...request,
+      user: user,
+      location: request.location,
+      pet: { ...request.pet, breed: request.pet.breed }
+    }
     try {
-      const postResponse = await this.repository.save(request, user)
+      const postResponse = await this.repository.save(post)
 
       return new PostResponse(
         postResponse._id,
