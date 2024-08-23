3424998f4edec40bc328aca6ae06cb6cdd9c71e0
Marcos Flavio <mfghost69@gmail.com>
2024-08-23 18:01:10 -0300

    Fix: check for null values returned by the populate function

--- app/src/module/post/provider/create.post.provider.ts
+++ app/src/module/post/provider/create.post.provider.ts
@@ -33,27 +33,44 @@ export class CreatePostProvider {
               }
             : { id: '', name: '' }
         },
-        user: {
-          name: post.user.name,
-          image: post.user.image,
-          description: post.user.description,
-          contact: post.user.contact
-            ? {
-                mail: post.user.contact.mail,
-                phone: post.user.contact.phone,
-                social: post.user.contact.mail
-              }
-            : {
+        user: post.user
+          ? {
+              name: post.user.name,
+              image: post.user.image,
+              description: post.user.description,
+              contact: post.user.contact
+                ? {
+                    mail: post.user.contact.mail,
+                    phone: post.user.contact.phone,
+                    social: post.user.contact.mail
+                  }
+                : {
+                    mail: '',
+                    phone: '',
+                    social: ''
+                  }
+            }
+          : {
+              name: '',
+              image: '',
+              description: '',
+              contact: {
                 mail: '',
                 phone: '',
                 social: ''
               }
-        },
-        location: {
-          id: post.location.id,
-          city: post.location.city,
-          state: post.location.state
-        }
+            },
+        location: post.location
+          ? {
+              id: post.location.id,
+              city: post.location.city,
+              state: post.location.state
+            }
+          : {
+              id: '',
+              city: '',
+              state: ''
+            }
       }
     } catch (e) {
       throw new InternalServerErrorException(e)
