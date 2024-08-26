e6196c76885800e85bbbbebd100fd2dc24eb29ab
Marcos Flavio <mfghost69@gmail.com>
2024-08-26 10:50:19 -0300

    Feat: initial commit

--- app/src/module/post/provider/get.post.provider.ts
+++ app/src/module/post/provider/get.post.provider.ts
@@ -6,49 +6,23 @@ import { PostRepository } from '../repository/post.repository'
 
 import { isNil } from 'lodash'
 
+@Injectable()
 export class GetPostProvider {
   constructor(private readonly repository: PostRepository) {}
 
   async run(id: string): Promise<PostResponse> {
-    const post = await this.repository.find()
+    const post = await this.repository.find({ id: id }).then((type) =>
+      type.populate([
+        { path: 'pet.breed', model: 'Breed' },
+        { path: 'user.contact', model: 'Contact' },
+        { path: 'location', model: 'Location' }
+      ])
+    )
 
     if (isNil(post)) {
       throw new NotFoundException('post not found')
     }
 
-    return {} as PostResponse
-
-    // return {
-    //   id: post.name,
-    //   title: post.name,
-    //   description: post.description,
-    //   image: post.image,
-    //   pet: {
-    //     name: post.pet.name,
-    //     age: post.pet.age,
-    //     size: post.pet.size,
-    //     gender: post.pet.gender,
-    //     breed: {
-    //       id: post.pet.breed.name,
-    //       name: post.pet.breed.name
-    //     }
-    //   },
-    //   location: {
-    //     id: '',
-    //     city: '',
-    //     state: ''
-    //   },
-    //   user: {
-    //     name: post.user.name,
-    //     image: post.user.image,
-    //     description: post.user.description,
-    //     contact: {
-    //       mail: post.user.contact.mail,
-    //       phone: post.user.contact.phone,
-
-    //       social: post.user.contact.social
-    //     }
-    //   }
-    // }
+    return new PostResponse(post)
   }
 }
