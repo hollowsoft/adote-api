48e81fdd6b577a08bbffd093176ef2c5c16367a3
Marcos Flavio <mfghost69@gmail.com>
2024-08-26 10:18:05 -0300

    Feat: initial commit

--- app/src/module/post/provider/list.post.provider.ts
+++ app/src/module/post/provider/list.post.provider.ts
@@ -12,41 +12,18 @@ export class ListPostProvider {
   async run(request: ListPostRequest): Promise<PostResponse[]> {
     const { size } = request
 
-    const list = await this.repository.list()
+    const list = await this.repository.list().then((posts) =>
+      Promise.all(
+        posts.map((post) =>
+          post.populate([
+            { path: 'pet.breed', model: 'Breed' },
+            { path: 'user.contact', model: 'Contact' },
+            { path: 'location', model: 'Location' }
+          ])
+        )
+      )
+    )
 
-    // return list.map((post) => ({
-    //   id: '',
-    //   title: post.name,
-    //   description: post.description,
-    //   image: post.image,
-    //   pet: {
-    //     name: post.pet.name,
-    //     age: post.pet.age,
-    //     size: post.pet.size,
-    //     gender: post.pet.gender,
-    //     breed: {
-    //       id: '',
-    //       name: post.pet.breed.name
-    //     }
-    //   },
-    //   location: {
-    //     id: '',
-    //     city: '',
-    //     state: ''
-    //   },
-    //   user: {
-    //     name: post.user.name,
-    //     image: post.user.image,
-    //     description: post.user.description,
-    //     contact: {
-    //       mail: post.user.contact.mail,
-    //       phone: post.user.contact.phone,
-
-    //       social: post.user.contact.social
-    //     }
-    //   }
-    // }))
-
-    return []
+    return list.map((post) => new PostResponse(post))
   }
 }
