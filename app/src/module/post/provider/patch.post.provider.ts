d36c2065f5c9f0c0364ca7888bcee669d43fd989
Marcos Flavio <mfghost69@gmail.com>
2024-08-26 17:37:47 -0300

    Feat: initial commit

--- app/src/module/post/provider/patch.post.provider.ts
+++ app/src/module/post/provider/patch.post.provider.ts
@@ -2,43 +2,20 @@ import { PostRepository } from '../repository/post.repository'
 
 import { PatchPostRequest } from '../post.request'
 import { PostResponse } from '../post.response'
+import { PatchPost } from '../repository/update.post.model'
 
 export class PatchPostProvider {
   constructor(private readonly repository: PostRepository) {}
 
   async run(id: string, request: PatchPostRequest): Promise<PostResponse> {
-    return {} as PostResponse
+    const post = this.repository.update({ _id: id }, new PatchPost(request)).then((type) =>
+      type.populate([
+        { path: 'pet.breed', model: 'Breed' },
+        { path: 'user.contact', model: 'Contact' },
+        { path: 'location', model: 'Location' }
+      ])
+    )
 
-    // return {
-    //   id: '',
-    //   title: '',
-    //   description: post.description,
-    //   image: post.image,
-    //   pet: {
-    //     name: post.pet.name,
-    //     age: post.pet.age,
-    //     size: post.pet.size,
-    //     gender: post.pet.gender,
-    //     breed: {
-    //       id: '',
-    //       name: post.pet.breed.name
-    //     }
-    //   },
-    //   location: {
-    //     id: '',
-    //     city: '',
-    //     state: ''
-    //   },
-    //   user: {
-    //     name: post.user.name,
-    //     image: post.user.image,
-    //     description: post.user.description,
-    //     contact: {
-    //       mail: post.user.contact.mail,
-    //       phone: post.user.contact.phone,
-    //       social: post.user.contact.social
-    //     }
-    //   }
-    // }
+    return new PostResponse(post)
   }
 }
