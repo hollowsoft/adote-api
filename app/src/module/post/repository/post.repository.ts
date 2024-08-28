25f09b889ee35ed718a9e4c72f101453aba7f3de
Marcos Flavio <mfghost69@gmail.com>
2024-08-28 11:34:46 -0300

    Fix: move populate to provider

--- app/src/module/post/repository/post.repository.ts
+++ app/src/module/post/repository/post.repository.ts
@@ -10,17 +10,8 @@ import { Post, PostDocument } from './post.schema'
 export class PostRepository {
   constructor(@InjectModel(Post.name) private model: Model<Post>) {}
 
-  async list(skip: number, size: number, query: object): Promise<PostDocument[]> {
-    return this.model
-      .find(query)
-      .skip(skip)
-      .limit(size)
-      .populate([
-        { path: 'pet.breed', model: 'Breed' },
-        { path: 'user.contact', model: 'Contact' },
-        { path: 'location', model: 'Location' }
-      ])
-      .exec()
+  async list(skip: number, size: number, query: FilterQuery<Post>): Promise<PostDocument[]> {
+    return this.model.find(query).skip(skip).limit(size).exec()
   }
 
   find(): Promise<PostDocument> {
