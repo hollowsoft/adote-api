10b1b9cbc838329a111c838bb6bdcb3db3f7462c
Marcos Flavio <mfghost69@gmail.com>
2024-08-29 18:21:07 -0300

    Fix: some fixes in provider

--- app/src/module/fav/provider/remove.fav.ts
+++ app/src/module/fav/provider/remove.fav.ts
@@ -1,13 +1,19 @@
 import { RemoveFavRequest } from '../fav.request'
 import { FavRepository } from '../fav.repository'
 import { Types } from 'mongoose'
+import { UserToken } from '@/type/auth.type'
+import { InternalServerErrorException } from '@nestjs/common'
 
 export class RemoveFavProvider {
   constructor(readonly repository: FavRepository) {}
 
-  async run(request: RemoveFavRequest, user: string): Promise<void> {
+  async run(request: RemoveFavRequest, user: UserToken): Promise<void> {
     const { id } = request
 
-    await this.repository.remove(new Types.ObjectId(id), { user })
+    try {
+      await this.repository.remove(new Types.ObjectId(id), { _id: user.id })
+    } catch (e) {
+      throw new InternalServerErrorException(e)
+    }
   }
 }
