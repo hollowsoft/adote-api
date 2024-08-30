e104bb9e5c016034e02d008894a4200d1ff89d37
Marcos Flavio <mfghost69@gmail.com>
2024-08-30 02:31:38 -0300

    Fix: rename

--- app/src/module/user/provider/patch.user.provider.ts
+++ app/src/module/user/provider/patch.user.provider.ts
@@ -7,9 +7,9 @@ import { PatchUser } from '../patch.user.model'
 export class PatchUserProvider {
   constructor(private readonly repository: UserRepository) {}
 
-  async run(request: PatchUserRequest, userId: string): Promise<UserResponse> {
+  async run(request: PatchUserRequest, id: string): Promise<UserResponse> {
     const user = await this.repository
-      .update(new PatchUser(request), { name: request.name })
+      .update(new PatchUser(request), { id: id })
       .then((type) => type.populate([{ path: 'location', model: 'Location' }]))
 
     return new UserResponse(user)
