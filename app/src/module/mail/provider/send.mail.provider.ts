f3ed4e7dc8bd78d59d535bb13cbd0ba88644e175
Igor Morais <igor.morais@hollowsoft.com>
2024-10-02 20:45:33 -0300

    some adjustments on send mail provider

--- app/src/module/mail/provider/send.mail.provider.ts
+++ app/src/module/mail/provider/send.mail.provider.ts
@@ -1,54 +1,37 @@
+import { Logger } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 
-import AWS from 'aws-sdk'
+import { SendEmailCommand, SESClient } from '@aws-sdk/client-ses'
 
 export class SendMailProvider {
+  private readonly logger = new Logger(SendMailProvider.name)
+
+  private readonly client = new SESClient()
+
   constructor(private readonly config: ConfigService) {}
 
-  async run(body: string, subject: string, to: string) {
-    const sender = this.config.get<string>('SENDER_EMAIL')
-    const secretKey = this.config.get<string>('SECRET_ACCESS_KEY')
-    const accessKey = this.config.get<string>('ACCESS_KEY_ID')
-    const awsRegion = this.config.get<string>('AWS_REGION')
-
-    AWS.config.update({
-      region: awsRegion,
-      credentials: {
-        secretAccessKey: secretKey,
-        accessKeyId: accessKey
-      }
-    })
-
-    const params = {
-      Destination: {
-        CcAddresses: [to],
-        ToAddresses: [to]
-      },
-      Message: {
-        Body: {
-          // Html: {
-          //   Charset: 'UTF-8',
-          //   Data: body
-          // },
-          Text: {
-            Charset: 'UTF-8',
-            Data: body
-          }
-        },
-        Subject: {
-          Charset: 'UTF-8',
-          Data: subject
-        }
-      },
-      Source: sender
-    }
-
+  async run(to: string, subject: string): Promise<void> {
     try {
-      const sendMailResponse = await new AWS.SES({ apiVersion: '2010-12-01' }).sendEmail(params).promise()
+      const param = {
+        Source: this.config.get<string>(''),
+        Destination: {
+          ToAddresses: [to]
+        },
+        Message: {
+          Subject: {
+            Data: subject
+          },
+          Body: {
+            Html: {
+              Data: ''
+            }
+          }
+        }
+      }
 
-      console.log(sendMailResponse.MessageId)
+      await this.client.send(new SendEmailCommand(param))
     } catch (e) {
-      console.error(`Failed to send email: Error: ${e}`)
+      this.logger.error(e)
     }
   }
 }
