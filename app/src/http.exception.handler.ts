685a325bda6eb57e258e2eb0ece2136ff1e8a10e
Igor Morais <igor.morais@hollowsoft.com>
2025-01-01 20:52:23 -0300

    some adjustments on exception handler

--- app/src/http.exception.handler.ts
+++ app/src/http.exception.handler.ts
@@ -6,15 +6,14 @@ import type { FastifyReply, FastifyRequest } from 'fastify'
 export class HttpExceptionHandler implements ExceptionFilter<HttpException> {
   private readonly logger = new Logger(HttpExceptionHandler.name)
 
-  catch(e: HttpException, argument: ArgumentsHost) {
+  catch(e: HttpException, t: ArgumentsHost) {
     this.logger.error(e)
 
-    const context = argument.switchToHttp()
-
-    const request = context.getRequest<FastifyRequest>()
-    const response = context.getResponse<FastifyReply>()
-
     const status = e.getStatus()
+
+    const request = t.switchToHttp().getRequest<FastifyRequest>()
+
+    const response = t.switchToHttp().getResponse<FastifyReply>()
 
     response.status(status).send({
       path: request.url,
